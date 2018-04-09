491743c8a3d9604ba745b2fec1cf4e782b6fdd73
Kinya TERASAKA <kinya.terasaka@visasq.com>
2018-04-09 23:29:38 +0900

    kill deps

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ Docs @ https://django-rest-swagger.readthedocs.io/
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 install_requires = [
-    'Django>=1.8',
-    'djangorestframework>=2.3.8',
+    # 'Django>=1.8',
+    # 'djangorestframework>=2.3.8',
     'PyYAML>=3.10',
 ]
 
